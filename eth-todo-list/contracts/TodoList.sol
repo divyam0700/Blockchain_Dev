e8878e6ac614548ddaa75cefe48d054db9d092e7
divyam0700 <54769607+divyam0700@users.noreply.github.com>
2021-12-17 02:38:39 +0530

    update

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -5,21 +5,39 @@ contract TodoList {
   uint public taskCount = 0;
 
   struct Task {
-      uint id;
-      string content;
-      bool completed;
+    uint id;
+    string content;
+    bool completed;
   }
-
 
   mapping(uint => Task) public tasks;
 
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
+
+  event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
   constructor() public {
-    createTask("Go, do something else.!");
+    createTask("Go!, do something else.");
   }
 
   function createTask(string memory _content) public {
     taskCount ++;
     tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
   }
 
 }
